eeea3778667279e82684291722763a328cd19beb
Annie Phan <anniephan.atp@gmail.com>
2019-09-03 16:55:31 -0700

    convert weather component to a presentational component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
-class Weather extends React.Component {
-  render() {
-    return (
+const Weather = (props) => {
+  return (
       <div>
-        { this.props.city && this.props.country && <h3>Location: {this.props.city}, {this.props.country}</h3> }
-        { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-        { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-        { this.props.description && <p>Conditions: {this.props.description}</p>}
-        { this.props.error && <p>{this.props.error}</p>}
+        { props.city && props.country && <h3>Location: {props.city}, {props.country}</h3> }
+        { props.temperature && <p>Temperature: {props.temperature}</p> }
+        { props.humidity && <p>Humidity: {props.humidity}</p>}
+        { props.description && <p>Conditions: {props.description}</p>}
+        { props.error && <p>{props.error}</p>}
       </div>
     )
-  }
 }
+
+// class Weather extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         { this.props.city && this.props.country && <h3>Location: {this.props.city}, {this.props.country}</h3> }
+//         { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
+//         { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+//         { this.props.description && <p>Conditions: {this.props.description}</p>}
+//         { this.props.error && <p>{this.props.error}</p>}
+//       </div>
+//     )
+//   }
+// }
 
 export default Weather
